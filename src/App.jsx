d6af2a61ec33fbaddb1ac016bf4c7f3e90127510
vicalcaz <47284013+vicalcaz@users.noreply.github.com>
2025-05-01 21:27:20 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ItemListContainer from './components/ItemListContainer'
-import NavBar from './components/NavBar';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <>
-    <Navbar/>
+    <NavBar/>
    
     <ItemListContainer greeting="¡Bienvenidos a mi cofre de tesoros!"/>
     </>
